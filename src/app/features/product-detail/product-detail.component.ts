ecdc0cc1b2b7156b7720a3df1755ebab1c3ee382
Uedney Morais <uedneymorais@gmail.com>
2025-06-14 12:49:16 -0300

    Configured platform_server to product_detail

--- src/app/features/product-detail/product-detail.component.ts
+++ src/app/features/product-detail/product-detail.component.ts
@@ -1,6 +1,6 @@
-import { CommonModule, CurrencyPipe } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { CommonModule, CurrencyPipe, isPlatformBrowser } from '@angular/common';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { Observable, map } from 'rxjs';
 import { Product, productFromDto } from '../../core/models/product.model';
 import { ProductService } from '../../core/service/product/product.service';
@@ -10,28 +10,35 @@ import { getFullPathImage } from '../../core/utils/url-path.utils';
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [CommonModule, CurrencyPipe],
+  imports: [CommonModule, CurrencyPipe, RouterModule],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.scss'
 })
 export class ProductDetailComponent implements OnInit {
   product$: Observable<Product>;
   currentUrl: string = '';
+  fullImagePath!: string;
+  private isBrowser: boolean;
 
   constructor(
     private route: ActivatedRoute,
     private productService: ProductService,
-    private seoService: SeoService
+    private seoService: SeoService,
+    @Inject(PLATFORM_ID) private platformId: Object
   ) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+
     this.product$ = this.productService
       .getProductBySlug(this.route.snapshot.params['slug'])
       .pipe(map(productFromDto));
   }
 
-  fullImagePath!: string;
+
   ngOnInit() {
-    if (typeof window !== 'undefined') {
+    if (this.isBrowser) {
       this.currentUrl = window.location.href;
+    } else {
+      this.currentUrl = 'URL não disponível no servidor para este contexto.';
     }
 
     this.product$.subscribe(product => {
